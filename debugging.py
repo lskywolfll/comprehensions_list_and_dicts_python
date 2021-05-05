d18a4f3d5016da5b20c6d56cdc4efa7c61fc00f6
Rene <renealejandrosanchezmorales@gmail.com>
2021-05-04 20:45:23 -0400

    performance code in debugging file, aplicated methodology exceptions and handle input value on terminal

--- debugging.py
+++ debugging.py
@@ -1,8 +1,23 @@
+# from utils.inputs import get_input
 
 divisors = lambda num: [i for i in range(1, num + 1) if num % i == 0]
 
+def get_input(msg):
+    while True:
+        try:
+            result = int(input(msg))
+
+            if result < 0:
+                print("No se admiten numeros negativos 😭\n")
+                raise ValueError()
+            else:
+                return result
+
+        except ValueError:
+            print("Por favor ingresar un numero valido")
+
 def run():
-    num = int(input("Ingresa un numero: "))
+    num = get_input("Ingresa un numero: ")
     print(divisors(num))
     print("Termino mi programa")
 
